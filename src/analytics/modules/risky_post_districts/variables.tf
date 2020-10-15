de086621a3b680c76d23124e285bb71d3e4fca6d
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-15 23:43:22 +0100

    Adding types to variables

--- src/analytics/modules/risky_post_districts/variables.tf
+++ src/analytics/modules/risky_post_districts/variables.tf
@@ -1,11 +1,14 @@
 variable "database_name" {
   description = "The name of the database this module should create"
+  type        = string
 }
 
 variable "workgroup_name" {
   description = "The name of the workspace associated to the database"
+  type        = string
 }
 
 variable "risky_post_codes_bucket_id" {
   description = "The name of the bucket the stores risky post codes"
+  type        = string
 }
