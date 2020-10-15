e84940e28311add80fb6c36bd5e69e5b5e6baf71
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-15 23:41:53 +0100

    Adding types to variables

--- src/analytics/modules/mobile_analytics/variables.tf
+++ src/analytics/modules/mobile_analytics/variables.tf
@@ -1,11 +1,14 @@
 variable "database_name" {
   description = "The name of the database this module should create"
+  type        = string
 }
 
 variable "workgroup_name" {
   description = "The name of the workspace associated to the database"
+  type        = string
 }
 
 variable "analytics_submission_store_parquet_bucket_id" {
   description = "The name of the bucket the stores mobile analytics"
+  type        = string
 }
