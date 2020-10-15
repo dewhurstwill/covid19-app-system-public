6e4d976cf6e001c446dd8653c032c02cf6aa4017
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-15 23:44:13 +0100

    Adding types to variables

--- src/analytics/variables.tf
+++ src/analytics/variables.tf
@@ -1,15 +1,19 @@
 variable "force_destroy_s3_buckets" {
   description = "Force destroy s3 buckets if set to true"
+  type        = string
 }
 
 variable "logs_bucket_id" {
   description = "The name of the bucket to which all S3 access logs are saved"
+  type        = string
 }
 
 variable "risky_post_codes_bucket_id" {
   description = "The name of the bucket the stores risky post codes"
+  type        = string
 }
 
 variable "analytics_submission_store_parquet_bucket_id" {
   description = "The name of the bucket the stores mobile analytics"
+  type        = string
 }
