22ed864b7f8c44f4d71ae9490720fba54181fef8
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-15 23:37:57 +0100

    Adding types to variables

--- src/analytics/libraries/analytics_s3/variables.tf
+++ src/analytics/libraries/analytics_s3/variables.tf
@@ -1,15 +1,19 @@
 variable "name" {
   description = "The name you want to use to identify this component in the system."
+  type        = string
 }
 
 variable "service" {
   description = "The name of a component providing a certain service. This could be another App-System service or an AWS service."
+  type        = string
 }
 
 variable "logs_bucket_id" {
   description = "The name of the bucket to which S3 access logs are saved."
+  type        = string
 }
 
 variable "force_destroy_s3_buckets" {
   description = "Force destroy s3 bucket if set to true"
+  type        = bool
 }
