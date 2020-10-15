b813c4952c8230b7363ddb5d931743d192b7fb3a
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-15 23:39:42 +0100

    Adding types to variables

--- src/analytics/modules/app_store_qr_posters/variables.tf
+++ src/analytics/modules/app_store_qr_posters/variables.tf
@@ -1,19 +1,24 @@
 variable "database_name" {
   description = "The name of the database this module should create"
+  type        = string
 }
 
 variable "workgroup_name" {
   description = "The name of the workspace associated to the database"
+  type        = string
 }
 
 variable "service" {
   description = "The name of a component providing a certain service. This could be another App-System service or an AWS service."
+  type        = string
 }
 
 variable "logs_bucket_id" {
   description = "The name of the bucket to which S3 access logs are saved."
+  type        = string
 }
 
 variable "force_destroy_s3_buckets" {
   description = "Force destroy s3 bucket if set to true"
+  type        = bool
 }
