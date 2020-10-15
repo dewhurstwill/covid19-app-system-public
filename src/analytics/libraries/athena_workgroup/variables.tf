7dfab83f24344662ce3e1665fd918d1fdbca7467
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-15 23:38:54 +0100

    Adding types to variables

--- src/analytics/libraries/athena_workgroup/variables.tf
+++ src/analytics/libraries/athena_workgroup/variables.tf
@@ -1,7 +1,9 @@
 variable "athena_output_store" {
   description = "The name of the bucket where Athena query results are stored"
+  type        = string
 }
 
 variable "name" {
   description = "The name you want to use to identify this workgroup in Athena."
+  type        = string
 }
